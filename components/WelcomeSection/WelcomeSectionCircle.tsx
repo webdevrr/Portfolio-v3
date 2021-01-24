a0307c2ea0dd191afefb6d90d51da22137b5e3fd
Robert <robert.ryszka.krr@gmail.com>
2021-01-24 15:04:57 +0100

    Added styles to WelcomeSectionCircle

--- components/WelcomeSection/WelcomeSectionCircle.tsx
+++ components/WelcomeSection/WelcomeSectionCircle.tsx
@@ -1,9 +1,44 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin-top: 5%;
+  width: 40%;
+  height: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+const AbsoluteOverflow = styled.span`
+  position: absolute;
+  overflow: hidden;
+  top: 20%;
+  right: 0;
+`;
+const Contact = styled.h3`
+  cursor: pointer;
+  color: white;
+  font-size: 2.2rem;
+`;
+const MiddleButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  cursor: pointer;
+  color: white;
+`;
+const Overflow = styled.span`
+  overflow: hidden;
+`;
+const MiddleTitle = styled.span`
+  display: block;
+`;
 const WelcomeSectionCircle = () => {
   return (
-    <div>
-      <span>
-        <h3>CONTACT</h3>
-      </span>
+    <Wrapper>
+      <AbsoluteOverflow>
+        <Contact>CONTACT</Contact>
+      </AbsoluteOverflow>
       <svg width="403px" height="403px" viewBox="0 0 403 403">
         <circle
           cx="50%"
@@ -14,15 +49,15 @@ const WelcomeSectionCircle = () => {
           stroke="gray"
         />
       </svg>
-      <div>
-        <span>
-          <span>check</span>
-        </span>
-        <span>
-          <span>projects</span>
-        </span>
-      </div>
-    </div>
+      <MiddleButton>
+        <Overflow>
+          <MiddleTitle>check</MiddleTitle>
+        </Overflow>
+        <Overflow>
+          <MiddleTitle>projects</MiddleTitle>
+        </Overflow>
+      </MiddleButton>
+    </Wrapper>
   );
 };
 
