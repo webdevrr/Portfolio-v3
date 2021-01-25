a1acd32150bb66107442617d7495ea9194e3b3f5
Robert <robert.ryszka.krr@gmail.com>
2021-01-25 19:51:52 +0100

    Added ProjectsSection

--- components/ProjectsSection/ProjectsSection.tsx
+++ components/ProjectsSection/ProjectsSection.tsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  width: 100%;
+  height: 100vh;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
 const ProjectsSection = () => {
-  return <div></div>;
+  return <Section></Section>;
 };
 
 export default ProjectsSection;
