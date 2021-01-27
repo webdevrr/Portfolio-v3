660b6e2559cc338a2b1bfa634d49eea4d6d1f7a7
Robert <robert.ryszka.krr@gmail.com>
2021-01-27 18:19:35 +0100

    Removed current projects section

--- components/ProjectsSection/List.tsx
+++
@@ -1,7 +0,0 @@
-import projects from "../../models/projects";
-
-const List = () => {
-  return <div></div>;
-};
-
-export default List;
--- components/ProjectsSection/ProjectsSection.tsx
+++ components/ProjectsSection/ProjectsSection.tsx
@@ -1,28 +1,7 @@
 import styled from "styled-components";
 
-const Section = styled.section`
-  width: 100%;
-  height: 100vh;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-`;
-const Projects = styled.div`
-  width: 70%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-`;
-const Title = styled.h1``;
 const ProjectsSection = () => {
-  return (
-    <Section>
-      <Projects></Projects>
-    </Section>
-  );
+  return <></>;
 };
 
 export default ProjectsSection;
