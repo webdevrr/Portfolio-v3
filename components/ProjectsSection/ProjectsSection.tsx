e996983153d32e3d25237af9aa854728f9ccd7e5
Robert <robert.ryszka.krr@gmail.com>
2021-01-31 15:00:32 +0100

    Fixed pin end trigger

--- components/ProjectsSection/ProjectsSection.tsx
+++ components/ProjectsSection/ProjectsSection.tsx
@@ -24,7 +24,7 @@ const ProjectsSection = () => {
       markers: true,
       trigger: sectionRef.current,
       start: "top",
-      endTrigger: "bottom bottom",
+      endTrigger: "bottom",
       pin: projectDescRef.current,
     });
   }, []);
