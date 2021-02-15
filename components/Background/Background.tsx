6124987fba44fb8182528ef4c5bc6fc1deed7d19
Robert <robert.ryszka.krr@gmail.com>
2021-02-15 19:20:49 +0100

    Added function to calculate position on scene

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,7 +1,10 @@
-import { useRef } from "react";
+import { useEffect } from "react";
 import { Canvas } from "react-three-fiber";
 import Box from "./Box";
+import { calculatePositionsOnCircle } from "../../utils/helpers";
+
 const Background = () => {
+  console.log(calculatePositionsOnCircle(2, 12));
   return (
     <>
       <Canvas style={{ position: "fixed", backgroundColor: "#1b1b1d" }}>
---
+++ utils/helpers.ts
@@ -0,0 +1,14 @@
+export const calculatePositionsOnCircle = (
+  numberOfPoints: number,
+  radius: number
+) => {
+  const positions = [];
+
+  for (let i = 0; i < numberOfPoints; i++) {
+    var x = radius * Math.cos((2 * Math.PI * i) / numberOfPoints);
+    var y = radius * Math.sin((2 * Math.PI * i) / numberOfPoints);
+
+    positions.push([Math.round(x), Math.round(y)]);
+  }
+  return positions;
+};
