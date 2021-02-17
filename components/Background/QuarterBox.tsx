4d37f3e28e1782213d7edfa4985763c206680781
Robert <robert.ryszka.krr@gmail.com>
2021-02-17 19:31:28 +0100

    Finished QuarterBox

--- components/Background/QuarterBox.tsx
+++ components/Background/QuarterBox.tsx
@@ -5,9 +5,9 @@ interface Side {
   rotation: [number, number, number];
 }
 const sides: Side[] = [
-  { position: [0, 0, 0], rotation: [0, 0, 0] },
-  { position: [0, 0, 0], rotation: [0, 0, 0] },
-  { position: [0, 0, 0], rotation: [0, 0, 0] },
+  { position: [0, 0, 0.25], rotation: [0, 0, 0] },
+  { position: [0, 0.25, 0], rotation: [Math.PI / 2, 0, 0] },
+  { position: [0.25, 0, 0], rotation: [0, Math.PI / 2, 0] },
 ];
 const QuarterBox = (props: Props) => {
   return (
