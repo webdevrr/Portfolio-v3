82888f1f5bb0a39d7a665a57f256568a30fe929a
Robert <robert.ryszka.krr@gmail.com>
2021-02-25 12:28:20 +0100

    Removed box

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,25 +1,13 @@
 import { Canvas } from "react-three-fiber";
-import ProjectCube from "./ProjectCube";
-import { calculatePositionsOnCircle } from "../../utils/helpers";
+
 import { OrbitControls } from "@react-three/drei";
 import projects from "../../models/projects";
-///[width,height,depth] - all should be proportional
-const cubeDimensions: [number, number, number] = [1, 1, 1];
 
 const Background = () => {
   return (
     <>
       <Canvas style={{ position: "fixed", backgroundColor: "#1b1b1d" }}>
         <ambientLight />
-        {calculatePositionsOnCircle(projects.length, 5).map((coords, index) => {
-          return (
-            <ProjectCube
-              cubeDimensions={cubeDimensions}
-              position={coords}
-              key={index}
-            />
-          );
-        })}
         <gridHelper args={[30, 30, 30]} />
         <axesHelper args={[10]} />
         <OrbitControls />
