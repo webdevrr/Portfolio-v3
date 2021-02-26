a020096e1e764b01c636afeebc9f6e613736e8be
Robert <robert.ryszka.krr@gmail.com>
2021-02-27 00:42:54 +0100

    Added patch to fix react spring core bug

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ const withTM = require("next-transpile-modules")([
   "@react-three/drei",
   "three",
 ]);
-config.module.rules.push({
-  test: /react-spring/,
-  sideEffects: true,
-});
+
 module.exports = withTM();
