200d8dae0e9955c3b595ef5a1903f4652d90f372
Robert <robert.ryszka.krr@gmail.com>
2021-02-27 14:21:46 +0100

    Added react-spring web

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
 import styled from "styled-components";
 import { useState } from "react";
+import { a, useSpring } from "@react-spring/web";
+
 import WelcomeSection from "../components/WelcomeSection/WelcomeSection";
 import ContactSection from "../components/ContactSection/ContactSection";
-
 import Header from "../components/Header";
 
-const Main = styled.main`
-  background-color: #1b1b1d;
-`;
+const Main = styled(a.main)``;
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
+  const [{ background }] = useSpring(
+    { background: isDarkMode ? "#1b1b1d" : "#f0f0f0" },
+    [isDarkMode]
+  );
+
   return (
     <>
       <Header />
-      <Main>
+      <Main style={{ background }}>
         <WelcomeSection isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
         <ContactSection />
       </Main>
